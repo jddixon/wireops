e843c69ac64466400e174f5a630fc85168db62e7
Jim Dixon <jddixon@gmail.com>
2017-08-11 14:56:43 -0700

    add tox.ini. *requirements.txt; edit cfg files

--- src/wireops/__init__.py
+++ src/wireops/__init__.py
@@ -2,8 +2,8 @@
 
 """ Low-level primitives for transmission of compressed data. """
 
-__version__ = '0.2.4'
-__version_date__ = '2017-07-30'
+__version__ = '0.2.5'
+__version_date__ = '2017-08-11'
 
 
 __all__ = ['__version__', '__version_date__', 'WireopsError']
