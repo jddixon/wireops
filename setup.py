7bb89c2ae731f0fd4587d17f1c842b490b05a503
Jim Dixon <jddixon@gmail.com>
2018-03-23 15:01:13 -0700

    fix corrupt .dvcz/builds

--- setup.py
+++ setup.py
@@ -6,30 +6,34 @@
 from os.path import exists
 from setuptools import setup
 
-LONG_DESC = None
+long_desc = None
 if exists('README.md'):
     with open('README.md', 'r') as file:
-        LONG_DESC = file.read()
+        long_desc = file.read()
 
 setup(name='wireops',
-      version='0.2.10',
+      version='0.2.11',
       author='Jim Dixon',
       author_email='jddixon@gmail.com',
-      long_description=LONG_DESC,
+      long_description=long_desc,
       packages=['wireops'],
       package_dir={'': 'src'},
       py_modules=[],
       include_package_data=False,
       zip_safe=False,
       scripts=[],
+      ext_modules=[],
       description='python3 protocol for compressed data transfer',
       url='https://jddixon.github.io/wireops',
       classifiers=[
           'Development Status :: 3 - Alpha',
           'Intended Audience :: Developers',
-          'License :: OSI Approved :: Apache Software License',
+          'License :: OSI Approved :: MIT License',
           'Natural Language :: English',
+          'Programming Language :: Python',
+          'Programming Language :: Python 2',
           'Programming Language :: Python 2.7',
+          'Programming Language :: Python 3',
           'Programming Language :: Python 3.5',
           'Programming Language :: Python 3.6',
           'Programming Language :: Python 3.7',
--- src/wireops/__init__.py
+++ src/wireops/__init__.py
@@ -2,8 +2,8 @@
 
 """ Low-level primitives for transmission of compressed data. """
 
-__version__ = '0.2.10'
-__version_date__ = '2018-02-27'
+__version__ = '0.2.11'
+__version_date__ = '2018-03-23'
 
 
 __all__ = ['__version__', '__version_date__', 'WireopsError']
